88d043b6c5625c9c1722827844e3a888762accb4
NINJAMZX <156536891+NINJAMZX@users.noreply.github.com>
2024-01-23 17:37:29 -0300

    creat

---
+++ main.js
@@ -0,0 +1,14 @@
+function para(impar, par){
+    return impar + par
+}
+var parado = para(2, 2)
+console.log(parado)
+
+function pedroGay(paradao){
+    if(paradao %2 == 0){
+        console.log(`par`)
+    }else{
+    console.log(`impar`)}
+ }
+
+
